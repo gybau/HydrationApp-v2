9be8d737666ab19d984afcd0a0df53d122f5185e
Michał Ganiebny <michalganiebny@rocketmail.com>
2022-11-23 10:54:36 +0100

    Initial Commit

---
+++ Hydration App/Hydration_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hydration_AppApp.swift
+//  Hydration App
+//
+//  Created by Michał Ganiebny on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hydration_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
